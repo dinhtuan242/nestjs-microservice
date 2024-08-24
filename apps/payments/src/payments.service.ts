74ea1052b9b6be5bea54bb8196753fcc6ceba495
dinhtuan242 <vudinhtuan242@gmail.com>
2024-08-24 11:02:09 +0700

    notification

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,13 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import Stripe from 'stripe';
-import { CreateChargeDto } from '@app/common';
+import { NOTIFICATION_SERVICE } from '@app/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { PaymentCreateChargeDto } from './dto/payment-create-charge.dto';
 
 @Injectable()
 export class PaymentsService {
   private stripe: Stripe;
 
-  constructor(private readonly configService: ConfigService) {
+  constructor(
+    private readonly configService: ConfigService,
+    @Inject(NOTIFICATION_SERVICE)
+    private readonly notificationService: ClientProxy,
+  ) {
     const secretKey = this.configService.get<string>('STRIPE_SECRET_KEY');
     if (!secretKey) {
       throw new Error('Stripe secret key not defined');
@@ -17,16 +23,23 @@ export class PaymentsService {
     });
   }
 
-  async createCharge({ amount }: CreateChargeDto) {
+  async createCharge({ amount, email }: PaymentCreateChargeDto) {
     try {
       // Create a payment intent
-      return await this.stripe.paymentIntents.create({
+      const paymentIntent = await this.stripe.paymentIntents.create({
         payment_method: 'pm_card_visa',
         amount: amount * 100, // Convert amount to cents
         confirm: true,
         payment_method_types: ['card'],
         currency: 'usd',
       });
+
+      this.notificationService.emit('notify_email', {
+        email,
+        text: `Your payment of ${amount} USD has complete successfully`,
+      });
+
+      return paymentIntent;
     } catch (error) {
       console.error('Payment creation failed', error);
       throw error;
